21eb2f9671b87561ee61d439b38bf72f480c98b6
FakeMonday <fs.outbound762@slmails.com>
2024-12-15 14:46:11 +0000

    draw triangle on screen

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
-    color = 0, 0, 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, color)
+        
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         
-        # limits framerate
+        # limits framerate to 60 FPS
         dt = clock.tick(60) / 1000
         
-
 
 if __name__ == "__main__":
     main()
