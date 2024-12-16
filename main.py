111ddabdf3e3f6c0850a22d3bd406a785cd018f6
FakeSaturday <106278294+FakeSaturday@users.noreply.github.com>
2024-12-16 19:30:10 +0000

    add asteroids to appear on screen

--- main.py
+++ main.py
@@ -2,11 +2,20 @@ import pygame
 from constants import *
 from circleshape import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
@@ -15,9 +24,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
+        for object in updatable:
+            object.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+        
+        for object in drawable:
+            object.draw(screen)
+
         pygame.display.flip()
         
         # limits framerate to 60 FPS
